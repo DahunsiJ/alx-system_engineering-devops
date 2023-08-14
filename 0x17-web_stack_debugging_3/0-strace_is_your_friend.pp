a513ca94f704cc0e072327bc8888ab7af6eca61b
DahunsiJ <justusdahunsi6@gmail.com>
2023-08-14 01:46:04 +0100

    kbsh

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
-  command => 'apt-get install -y ruby && gem install puppet-lint -v 2.1.1 && sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
